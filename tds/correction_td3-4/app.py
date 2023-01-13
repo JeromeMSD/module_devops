48d66f9cadaf2bdaaf36f2a65f6d25c3ee3b6028
Jerome Massard <jerome.massard@kiowy.com>
2023-01-13 09:02:17 +0100

    Check syntax code

--- tds/correction_td3-4/app.py
+++ tds/correction_td3-4/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import sys
 
 app = Flask(__name__)
 
@@ -26,4 +27,11 @@ def hello_world():
 	return ret
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	if len(sys.argv) > 1:
+		if sys.argv[1] == "check_syntax":
+			print("Build [ OK ]")
+			exit(0)
+		else:
+			print("Passed argument not supported ! Supported argument : check_syntax")
+			exit(1)
+	app.run(debug=True)
